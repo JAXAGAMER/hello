c8fae3a702212f16712202d1e7e27cc8b95e55e8
JAXAGAMER <109780109+JAXAGAMER@users.noreply.github.com>
2024-12-24 16:51:12 +0500

    hello

---
+++ portfolio/src/components/ContactMeSection.js
@@ -0,0 +1,113 @@
+import React, {useEffect} from "react";
+import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import * as Yup from 'yup';
+import FullScreenSection from "./FullScreenSection";
+import useSubmit from "../hooks/useSubmit";
+import {useAlertContext} from "../context/alertContext";
+
+const ContactMeSection = () => {
+  const {isLoading, response, submit} = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      email: "",
+      type: "hireMe",
+      comment: "",
+    },
+    onSubmit: (values) =>{
+      submit("", values);
+      onOpen(response.type, response.message);
+      response.type === 'success' && formik.resetForm();
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      type: Yup.string().required("Required").optional(),
+      comment: Yup.string().required("Required").min(25, "Must be at least 25 characters"),
+  }),
+});
+
+
+
+  return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#512DA8"
+      py={16}
+      spacing={8}
+    >
+      <VStack w="100vw" p={32} alignItems="flex-start" px="20vw">
+        <Heading as="h1" id="contactme-section">
+          Contact me
+        </Heading>
+        <Box p={6} rounded="md" w="100%">
+          <form onSubmit = {(e) => {
+                e.preventDefault();
+                formik.handleSubmit();
+            }}>
+            <VStack spacing={4}>
+              <FormControl isInvalid={formik.touched.firstName && formik.errors.firstName}>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  {...formik.getFieldProps("firstName")}
+                />
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  {...formik.getFieldProps("email")}
+                />
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select id="type" name="type" {...formik.getFieldProps("type")} >
+                  <option value="hireMe" style={{ backgroundColor: '#512DA8'}}>Freelance project proposal</option>
+                  <option value="openSource" style={{ backgroundColor: '#512DA8'}}>
+                    Open source consultancy session
+                  </option>
+                  <option value="other" style={{ backgroundColor: '#512DA8'}}>Other</option>
+                </Select>
+              </FormControl>
+              <FormControl isInvalid={formik.touched.comment && formik.errors.comment}>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  {...formik.getFieldProps("comment")}
+                />
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+              </FormControl>
+              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </FullScreenSection>
+  );
+};
+
+export default ContactMeSection;
---
+++ portfolio/src/components/LandingSection.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Raul Allende!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack gap={4}>
+      <Avatar size="lg" src={require("../images/9.jpg")} />
+      <Heading size="md">{greeting}</Heading>
+      <Heading size="xl">{bio1}</Heading>
+      <Heading size="xl">{bio2}</Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
